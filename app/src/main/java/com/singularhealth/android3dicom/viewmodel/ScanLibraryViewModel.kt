2ec0d2b0e3ac669bc851c8dacbf5545d41ee79f9
IDEGROUP\luna.shin <luna.shin@ide.group>
2024-10-15 14:33:55 +1100

    user is directed to the external website for sign up

--- app/src/main/java/com/singularhealth/android3dicom/viewmodel/ScanLibraryViewModel.kt
+++ app/src/main/java/com/singularhealth/android3dicom/viewmodel/ScanLibraryViewModel.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.text.Typography.dagger
 
 @HiltViewModel
 class ScanLibraryViewModel
